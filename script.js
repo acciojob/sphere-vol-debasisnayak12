965b356406b85cc39d16545589293e7a74dcb22c
Debasis Nayak <115566899+debasisnayak12@users.noreply.github.com>
2023-11-24 10:58:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-function volume_sphere() {
+function volume_sphere(event) {
     //Write your code here
-  
+	// Prevent form submission and page reload
+	event.preventDefault();
+	// get radius from radius input 
+	const radiusField = document.getElementById("radius");
+	const radiusValue = parseFloat(radiusField.value);
+	
+	const findVolume = (4/3) * Math.PI * Math.pow(radiusValue, 3);
+
+	const showVolume = document.getElementById("volume");
+	showVolume.value = findVolume.toFixed(4);
+	 
+	// To prevent form submission
+	return false; 
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
