d0f7ad87b11b837fdbb2db1a52d8401f0cdab673
Debasis Nayak <115566899+debasisnayak12@users.noreply.github.com>
2023-11-24 16:03:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,10 +3,9 @@ function volume_sphere(event) {
 	// Prevent form submission and page reload
 	event.preventDefault();
 	// get radius from radius input 
-	const radiusField = document.getElementById("radius");
-	const radiusValue = parseFloat(radiusField.value);
+	const radiusField = document.getElementById("radius").value;
 	
-	const findVolume = (4/3) * Math.PI * Math.pow(radiusValue, 3);
+	const findVolume = (4/3) * Math.PI * Math.pow(radiusField, 3);
 
 	const showVolume = document.getElementById("volume");
 	showVolume.value = findVolume.toFixed(4);
